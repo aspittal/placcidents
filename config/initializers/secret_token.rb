353522f10d3d2d0d4b3f96a3076173d29957b61a
Andrew Spittal <aspittal@gmail.com>
2012-02-02 12:38:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Placcidents::Application.config.secret_token = '473b209bf6427c75c64a0a049a6b179ad1911328f2392fbf516c479c1f22e3d1c96b6a48af202a94bba7e31a83db39ad79e70186db41569b1b3ca4e0c0a53bf4'
