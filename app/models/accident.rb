74bf4997a409f22a487f4f4ec65dbffe01b10e46
Andrew Spittal <aspittal@gmail.com>
2012-02-14 16:29:28 -0700

    Fixed the migration and a couple of issues with the index view

--- app/models/accident.rb
+++ app/models/accident.rb
@@ -1,2 +1,6 @@
 class Accident < ActiveRecord::Base
+	geocoded_by :location
+	before_save :geocode
+
+	scope :has_coordinates, where('LENGTH(latitude > 0) AND LENGTH(longitude > 0)')	
 end
--- db/migrate/20120214203110_create_accidents.rb
+++ db/migrate/20120214203110_create_accidents.rb
@@ -1,8 +1,22 @@
 class CreateAccidents < ActiveRecord::Migration
-  def change
-    create_table :accidents do |t|
+  	def change
+    	create_table :accidents do |p|
+	    	p.string :checksum
+	        p.date :date
+	        p.string :location
+	        p.string :operator
+	        p.string :aircraft_type
+	        p.string :registration
+	        p.integer :fatalities
+	        p.integer :total_passengers
+	        p.integer :ground_fatalities
+	        p.float :latitude
+	        p.float :longitude
+	      	p.timestamps
+    	end
+  	end
 
-      t.timestamps
-    end
-  end
+  	def down
+  		drop_table :accidents
+  	end
 end
